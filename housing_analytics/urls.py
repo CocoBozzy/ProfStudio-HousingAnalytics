af0d93b1cad8b677ed2132802122a59fbfca552b
Joel-M3 <50311672+Joel-M3@users.noreply.github.com>
2022-03-27 15:03:04 +1100

    Added Home View
    Home html template added

--- housing_analytics/urls.py
+++ housing_analytics/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#Added Home View
+from home import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home)
 ]
