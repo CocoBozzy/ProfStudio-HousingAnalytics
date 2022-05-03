b5f56c96ef1a73e12259d7fa45462e181e82ca6b
rachel.birrell <rachelb1374@gmail.com>
2022-05-03 19:52:37 +1000

    fix merge conflicts

--- manage.py
+++ manage.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-"""HousingAnalytics's command-line utility for administrative tasks."""
-=======
+
 """Django's command-line utility for administrative tasks."""
->>>>>>> origin/Joel-Test-Env
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HousingAnalytics.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'housing_analytics.settings')
->>>>>>> origin/Joel-Test-Env
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-<<<<<<< HEAD
-            "Couldn't import HousingAnalytics. Are you sure it's installed and "
-=======
+            "Couldn't import Django. Are you sure it's installed and "
+
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
