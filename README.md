a2e87d427f1a5bfd39e960b1569eda62123e68c6
Callum-F <callumfreeburn1996@gmail.com>
2022-10-05 19:08:34 +1100

    added code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Repository for the development of our groups Housing analytics project.
  
 venv\Scripts\activate
+
+python manage.py runserver
--- callumDomainTest.py
+++ callumDomainTest.py
@@ -1,9 +1,8 @@
 import requests
 import json
 
-rach_client = 'client_e4658c392bc206a4e6dfb1335576444b'
-rach_secret = 'secret_830a73c26ec95dc66c2d605e62809e08'
-rach_api_key = 'key_54d3141d923ee3353eab7288845f9f82'
+cal_client = 'client_f49317aa4d716d953f138865e1f399d9'
+cal_secret = 'secret_dcffc2565d1043fd13ac488f3e522296'
 auth_url = 'https://auth.domain.com.au/v1/connect/token'
 url_test = 'https://api.domain.com.au/v1/agencies/22473/listings?listingStatusFilter=live&pageNumber=1&pageSize=3'
 listing_base_url = 'https://api.domain.com.au/v1/listings/'
@@ -11,9 +10,9 @@ listing_base_url = 'https://api.domain.com.au/v1/listings/'
 
 def get_access_token():
     resp = requests.post(auth_url, data={
-        'client_id': rach_client,
-        'client_secret': rach_secret,
-        'scope': 'api_listings_read',
+        'client_id': cal_client,
+        'client_secret': cal_secret,
+        'scope': 'api_suburbperformance_read',
         'grant_type': 'client_credentials',
         'Content-Type': 'text/json'
     })
@@ -25,14 +24,14 @@ def get_access_token():
 
 def get_salesResults(token , suburb = 'Sydney'):
     
-    url = 'https://api.domain.com.au/v1/salesResults/' + suburb
+    url = 'https://api.domain.com.au/v1/salesResults/Sydney/listings'
     
-    auth = {'X-Api-Key': token}
+    auth = {'Authorization': 'Bearer ' + token}
     
     resp = requests.get(url, headers=auth)
     print(resp.status_code)
     resp_json = resp.json()
-    print(resp_json)
+    # print(json.dumps(resp_json, indent=4))
     return resp_json
 
 
@@ -64,6 +63,42 @@ def get_agencies_listings(token, suburb, bedrooms=1, bathrooms=1, cars=1):
     print(resp_json)
     return resp_json
 
+######################################################################################################################################
+
+def suburbPerformance(token, propertyCategory, bedrooms, state, suburb, postcode):
+    
+    url = 'https://api.domain.com.au/v2/suburbPerformanceStatistics/' + state +'/' + suburb +'/' + str(postcode) + '?propertyCategory=' +  propertyCategory + '&bedrooms=' + str(bedrooms) +'&periodSize=years&startingPeriodRelativeToCurrent=1&totalPeriods=11'
+    
+    auth = {'Authorization': 'Bearer ' + token}
+    
+    
+    resp = requests.get(url, headers=auth)
+    print(resp.status_code)
+    resp_json = resp.json()
+    # print(json.dumps(resp_json, indent=4))
+    return resp_json
+    
+def extract_sold_in_year(json_response):
+    info = json_response['series']['seriesInfo']
+    print(json.dumps(info, indent=4))
+
+    yearSalesResultDict = {}
+    for i in info:
+        yearSalesResultDict[i['year']] = i['values']['numberSold']
+        
+    print(json.dumps(yearSalesResultDict, indent=4))
+    return yearSalesResultDict
+
+
+def get_sales_per_suburb_per_year(token, propertyCategory, bedrooms, state, suburb, postcode):
+    json_response = suburbPerformance(token, propertyCategory, bedrooms, state, suburb, postcode)
+    extract_sold_in_year(json_response)
+    
+    
+
 if __name__ == '__main__':
-    token = 'key_7a95da6a34c2d3c8b213bd5430f3016d'
-    get_salesResults(token)
+    token = get_access_token()
+    get_sales_per_suburb_per_year(token, propertyCategory='house', bedrooms=4, state='nsw', suburb='terrigal', postcode=2260)
+
+    
+
