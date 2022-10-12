da46afe08cb519ea7b7be0fb3156962fadf1dccc
CocoBozzy <cohenbos@gmail.com>
2022-10-12 19:34:01 +1100

    airflow DAG test

---
+++ DAGS/Ingest.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+# We need to import the operators used in the tasks
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+import pandas as pd
+
+
+# initializing the default arguments that we'll pass to our DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(5)
+}
+
+ingestion_dag = DAG(
+    'SydneyHousePrice_ingestion',
+    default_args=default_args,
+    description='Aggregates housing price records for analysis',
+    schedule_interval=timedelta(hours=1),
+    catchup=False
+)
+
+from airflow.operators.python_operator import PythonOperator
+
+task_1 = PythonOperator(
+    task_id='transform_data',
+    python_callable=transform_data,
+    dag=ingestion_dag,
+)
+
+task_2 = PythonOperator(
+    task_id='load_data',
+    python_callable=load_data,
+    dag=ingestion_dag,
+)
+
+def transform_data():
+    SydHousePrice = pd.read_csv(f"{dag_path}/raw_data/SydneyHousingPrice.csv", low_memory=False)
+
+    # make date format consistent
+    data.Date = pd.to_datetime(data.Date, infer_datetime_format=True)
+
+    # load processed data
+    data.to_csv(f"{dag_path}/processed_data/processed_data.csv", index=False)
