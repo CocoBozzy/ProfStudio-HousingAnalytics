0bb2e4c976369ae5fedbe71a803e4904f0b135f7
Joel-M3 <50311672+Joel-M3@users.noreply.github.com>
2022-10-17 15:04:29 +1100

    Added in Sydney Graph
    csv file to graph

--- charts/urls.py
+++ charts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('charts', views.ChartView.as_view(), name='charts'),
+    path('deluxe_charts', views.chart_data, name='premium_charts'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from .models import House
+import pandas as pd
+import json
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,6 +19,23 @@ class ChartView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["qs"]= House.objects.all()
         return context
+
+
+@login_required(login_url='login')
+def chart_data(request):
+    #Get CSV file
+    data = pd.read_csv("data_sources/SydneyHousing.csv")
+
+    #Convert columns to a list array
+    labels=  list(data['Suburb'])
+    datas = list(data['No.Sales'])
+
+    #Return context as safe json 
+    context = {"labels": mark_safe(json.dumps(labels)), "datas": mark_safe(json.dumps(datas))}
+
+    return render(request, 'charts/deluxe_charts.html',context)
+
+
 
 
 
